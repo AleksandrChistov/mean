8a913b4854f96c4d2ab8c0eed34f7d18b35da116
Alexander <aleksandrchistov.ru@yandex.ru>
2021-04-15 13:30:22 +0700

    Created auth form

--- front-end/src/app/reg/reg.component.ts
+++ front-end/src/app/reg/reg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-reg',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegComponent implements OnInit {
 
-  constructor() { }
+  public name: string;
+  public login: string;
+  public email: string;
+  public password: string;
+
+  constructor(private _flashMessagesService: FlashMessagesService) { }
 
   ngOnInit(): void {
   }
 
+  singUp() {
+    if (!this.checkAllFields()) {
+      return;
+    }
+
+    return {
+      name: this.name,
+      login: this.login,
+      email: this.email,
+      password: this.password,
+    }
+  }
+
+  checkAllFields(): boolean {
+    if (!this.name) {
+      this._flashMessagesService.show('Enter your name', { cssClass: 'alert-danger' });
+      return false;
+    }
+    if (!this.login) {
+      this._flashMessagesService.show('Enter your login', { cssClass: 'alert-danger' });
+      return false;
+    }
+    if (!this.email) {
+      this._flashMessagesService.show('Enter your email', { cssClass: 'alert-danger' });
+      return false;
+    }
+    if (!this.password) {
+      this._flashMessagesService.show('Enter your password', { cssClass: 'alert-danger' });
+      return false;
+    }
+
+    return true;
+  }
 }
